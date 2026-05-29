134e99fc3bcf1c3d900f00db702c4530c842219b
Juliusz Kopciński <juliusz.kopcinski@gmail.com>
2026-05-29 20:55:44 +0200

    zadanie 2 jwt Add RegisterController

---
+++ UslugiSieciowe/BlogCMS/Controllers/RegisterController.cs
@@ -0,0 +1,21 @@
+﻿using BlogCMS.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BlogCMS.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    [Authorize]
+    public class RegisterController : ControllerBase
+    {
+        [HttpPost]
+        public IActionResult Register(LoginModel newUser)
+        {
+            newUser.Role = "Admin";
+            UserConstants.Users.Add(newUser);
+
+            return Ok("User registered");
+        }
+    }
+}
